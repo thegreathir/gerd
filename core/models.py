6565c9ec32a0fbc49488bdfc6677093e53f53302
thegreathir <aaph1377@gmail.com>
2022-02-12 19:04:39 +0330

    Update models

--- core/models.py
+++ core/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 
-# Create your models here.
-
 
 class Word(models.Model):
     text = models.CharField(max_length=128)
@@ -17,10 +15,7 @@ class Word(models.Model):
 
 
 class Match4(models.Model):
-    players = models.ManyToManyField(
-        to=settings.AUTH_USER_MODEL,
-        blank=True,
-    )
+    room = models.ForeignKey("Room4", models.CASCADE)
 
     words = models.ManyToManyField(
         to=Word,
@@ -30,18 +25,38 @@ class Match4(models.Model):
     class State(models.IntegerChoices):
         NEWBORN = 1
         STARTED = 2
-        FINISHED = 3
+        PAUSED = 3
+        FINISHED = 4
 
     state = models.IntegerField(choices=State.choices)
     start_time = models.DateTimeField(blank=True, null=True)
     current_turn = models.IntegerField(blank=True, null=True)
 
     def clean(self, *args, **kwargs):
-        if self.players.count() > 4:
-            raise ValidationError(
-                "More than 4 players attached")
         if self.current_turn >= 4:
             raise ValidationError(
                 "Out of range turn")
+
+        super(Match4, self).clean(*args, **kwargs)
+
+
+class Room4(models.Model):
+    name = models.CharField(max_length=128)
+    players = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        blank=True,
+    )
+
+    class Teams(models.IntegerChoices):
+        ONE_TWO__THREE_FOUR = 1
+        ONE_THREE__TWO_FOUR = 2
+        ONE_FOUR__TWO_THREE = 3
+
+    teams = models.IntegerField(choices=Teams.choices)
+
+    def clean(self, *args, **kwargs):
+        if self.players.count() > 4:
+            raise ValidationError(
+                "More than 4 players attached")
 
         super(Match4, self).clean(*args, **kwargs)
