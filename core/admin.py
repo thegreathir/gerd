8881ca79af46f2e41819b77a359264c47e825e4d
thegreathir <aaph1377@gmail.com>
2022-02-12 20:06:50 +0330

    Add rest framework token auth

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Word
+from core.models import Match4
+from core.models import Room4
 
 # Register your models here.
+admin.site.register(Word)
+admin.site.register(Match4)
+admin.site.register(Room4)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
-from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class Word(models.Model):
@@ -60,3 +63,9 @@ class Room4(models.Model):
                 "More than 4 players attached")
 
         super(Match4, self).clean(*args, **kwargs)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
