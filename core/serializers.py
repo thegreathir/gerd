0b0dad441547bc266d5796ca2048fd133c958320
thegreathir <aaph1377@gmail.com>
2022-02-14 19:06:15 +0330

    Add match serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from core.models import Room
+from core.models import Room, Match
+
+
+class MatchSerializer(serializers.ModelSerializer):
+    words = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='text'
+    )
+    class Meta:
+        model = Match
+        fields = ['words', 'state', 'start_time', 'current_turn']
 
 
 class RoomSerializer(serializers.ModelSerializer):
@@ -10,6 +21,7 @@ class RoomSerializer(serializers.ModelSerializer):
         queryset=User.objects.all(),
         slug_field='username'
     )
+    match = MatchSerializer(many=False, read_only=True)
     class Meta:
         model = Room
-        fields = ['id', 'name', 'players', 'teams']
+        fields = ['id', 'name', 'players', 'teams', 'match']
